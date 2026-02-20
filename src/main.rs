755046b0d1872e9f1a20a066cb1dc622e945cd39
albanix <xutaoalbania@gmail.com>
2026-02-21 00:45:47 +0200

    Я читав документацію, та спростив свій код в рази

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ use std::sync::{
 use std::collections::VecDeque;
 use std::pin::Pin;
 use std::future::Future;
-use std::task::RawWaker;
+use std::task::{Waker, Wake};
+
 struct Executor {
     queue: Mutex<VecDeque<Arc<Task>>>, // VecDeque - швидше за звичайний Vector і парцює з push_front, push_back. Що є доволі зручним у контексті мого рантайму (vec - 6 O(n), VecDeque O(1)), вона надає FIFO, тому це зручно.
 }
@@ -15,13 +16,13 @@ struct Task {
 }
 
 impl Executor {
-    fn spawn<F>(&self, x: &Arc<Self>, future: F)
+    fn spawn<F>(self: &Arc<Self>, future: F)
     where
         F: Future<Output = ()> + Send + 'static
     {
         let task = Arc::new(Task {
             future: Mutex::new(Box::pin(future)),
-            executor: x.clone(),
+            executor: self.clone(),
         });
 
         while let Some(task) = self.queue.lock().unwrap().pop_front() {
@@ -30,30 +31,13 @@ impl Executor {
     }
 }
 
-fn waker() {
-
+impl Wake for Task {
+    fn wake(self: Arc<Self>) {
+        let exec = self.executor.clone();
+        let mut queue = exec.queue.lock().unwrap();
+        queue.push_back(self);
+    }
 }
-
-unsafe fn wake_by_ref(ptr: *const ()) {
-    let arc = unsafe {
-        Arc::<Task>::from_raw(ptr as *const Task)
-    };
-    arc.executor.queue.lock().unwrap().push_back(arc.clone());
-    std::mem::forget(arc);
-}
-
-unsafe fn drop(ptr: *const()) {
-    unsafe {
-        Arc::from_raw(ptr as *const Task)
-    };
-}
-
-fn clone(ptr: *const ()) -> RawWaker {
-    let arc = unsafe { Arc::<Task>::from_raw(ptr as *const Task) }; // відновлюмо Arc<Task> який прилетів з into_raw: ! кожен конкретний poiter, який був отриманний з into_raw має бути повернено через from_raw = рівно один раз ! 
-    let cloned = arc.clone();
-    std::mem::forget(cloned);
-    RawWaker::new(Arc::into_raw(cloned) as *const (), )
-}
-fn main()_{
-
+fn main() {
+    print!("123");
 }
